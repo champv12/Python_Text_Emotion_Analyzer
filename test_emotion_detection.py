d8db9c34157c6bcc2d9dc373068145336e22ffc7
champv12 <kittypaws875@gmail.com>
2024-12-25 03:22:19 -0500

    changes to test_emotion_detection

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,13 +1,9 @@
 #importing package
 from EmotionDetection.emotion_detection import emotion_detector
+import unittest
 
-#statement 1
-emotion_detector("I am glad this happened")
-#statement 2
-emotion_detector("I am really mad about this")
-#statement 3
-emotion_detector("I feel disgusted just hearing about this")
-#statement 4
-emotion_detector("I am so sad about this")
-#statement 5
-emotion_detector("I am really afraid that this will happen")
+class TestEmotionDetector(unittest.TestCase):
+    def test_emotion_detector(self):
+        result_1 = emotion_detector("")
+        self.assetEqual(result_1[])
+    
