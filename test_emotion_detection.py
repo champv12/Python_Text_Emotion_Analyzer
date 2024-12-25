a2d566cc5df76b31337ca3b600f096301045add0
champv12 <kittypaws875@gmail.com>
2024-12-25 03:08:50 -0500

    cloned missing repo and added test_emotion_detection.py

---
+++ test_emotion_detection.py
@@ -0,0 +1,13 @@
+#importing package
+from EmotionDetection.emotion_detection import emotion_detector
+
+#statement 1
+emotion_detector("I am glad this happened")
+#statement 2
+emotion_detector("I am really mad about this")
+#statement 3
+emotion_detector("I feel disgusted just hearing about this")
+#statement 4
+emotion_detector("I am so sad about this")
+#statement 5
+emotion_detector("I am really afraid that this will happen")
